67f40deaf581c15e2c88407424e8d47733fb8ef0
Christopher Collins <chris@ccollins.io>
2019-09-16 21:49:34 -0600

    Created HTML Structure for the quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // alert('test');
-  console.log("Questions",questions);
-  console.log("Answers",answers);
+  console.log("Questions", questions);
+  console.log("Answers", answers);
+
+  $('.custom-control-input').change(function () {
+    $('.custom-control-input').prop('checked', false);
+    $(this).prop('checked', true);
+  });
+
+  var loader = $('.loader').ClassyLoader({
+    animate: false,
+    percentage: 0,
+    speed: 20,
+    fontColor: '#1E2022',
+    fontSize: '50px',
+    diameter: 80,
+    lineColor: '#0092CA',
+    remainingLineColor: 'rgba(0, 146, 202, 0.3)', /* 'rgba(82, 97, 107, 0.7)', */ 
+    lineWidth: 10
+  });
+
+  setTimeout(loader.setPercent(50).draw,5 * 1000);
+
 });
