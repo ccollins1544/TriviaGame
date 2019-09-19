ba913989729a4d1fbfe84b5e1791555009941b2d
Christopher Collins <chris@ccollins.io>
2019-09-18 20:17:16 -0600

    Added Images. Cleaned up code on createQuestion and checkAnswer functions. Fixed on change bug with selecting multiple answers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@
  * @version 2.0
  * @license none (public domain)
  * 
+ * NOTE: the variables QUESTIONS and ANSWERS must be predefined in app-data.js
  * ===============[ TABLE OF CONTENTS ]===============
  * 0. Globals
  * 1. Functions
@@ -12,9 +13,26 @@
  *   1.2 shuffle_array
  *   1.3 runEffect
  *   1.4 createQuestion
+ *     1.4.1 ANSWER IMAGE
+ *     1.4.2 POSSIBLE ANSWERS
+ *     1.4.3 See if question has already been graded
+ *     1.4.4 COMBINED Answer Image with Possible Answers
+ *     1.4.5 COMBINED Answer Block with the question
+ * 
  *   1.5 checkAnswer
+ *     1.5.1 Fetch Submitted Answer
+ *     1.5.2 See if the answer is right
+ *     1.5.3 Save Results weather it was right or wrong.
+ *     1.5.4 Update HTML to reflect right and wrong answers
+ *     1.5.5 Calculate the score so far
+ * 
  *   1.6 nextQuestion
  *   1.7 previousQuestion
+ *   1.8 newGame
+ *     1.8.1 Reset Globals
+ *     1.8.2 Generate First question
+ *     1.8.3 Set Quiz Progress to 0%
+ *     1.8.3 Reset Progress Bars
  * 
  * 2. Document Ready
  *   2.1 Generate the first question
@@ -23,22 +41,21 @@
  *   2.4 Set Up Clickable elements
  * 
  * @todo
- * -Make so once the question has been graded the answer can't be changed.
  * -Add Timer on questions that auto-grades and moves on to the next question.
+ * -Add Score Page
+ * -Add New Game Feature
+ * -Decide if I really need PreviousQuestion or not...and when to show it if I keep it. 
+ * 
+ * @done 
+ * -Make so once the question has been graded the answer can't be changed.
  * -Detect if question has already been answered when viewing previous_question. 
  * -Update Correct/Wrong/Unanswered?
  * -Add Pictures and Questions
- * -Add New Game Feature
- * -Add Score Page
- * -Decide if I really need PreviousQuestion or not...and when to show it if I keep it. 
  * -Switch Score and % progress circle...maybe show a progress bar at the top instead. 
  *****************************************************/
-/* ===============[ 0. GLOBALS ]======================
-* NOTE: the variables QUESTIONS and ANSWERS must be predefined in the following script,
-* <script src="assets/javascript/app-data.js" type="text/javascript"></script>
-*/
-var SCORE = new Array(QUESTIONS.length); // array of booleans representing right/wrong answers to the questions.
-var SUBMITTED_ANSWERS = new Array(QUESTIONS.length); // Array of integers representing answers index.
+/* ===============[ 0. GLOBALS ]======================*/
+var SCORE = new Array(QUESTIONS.length);             // Empty array of booleans representing right/wrong answers to the questions.
+var SUBMITTED_ANSWERS = new Array(QUESTIONS.length); // Empty array of integers representing answers index.
 var QUIZ_PROGRESS = $('.loader');
 
 /* ===============[ 1. FUNCTIONS ]====================*/
@@ -73,6 +90,7 @@ function shuffle_array(some_array){
 function runEffect(element,selectedEffect="pulsate") {
   // Most effect types need no options passed by default
   var options = {};
+
   // some effects have required parameters
   if ( selectedEffect === "scale" ) {
     options = { percent: 50 };
@@ -93,81 +111,76 @@ function runEffect(element,selectedEffect="pulsate") {
  * 
  * @param {object} Q - question object with the properties: question, image, answer, wrong, topic, subtopic
  * @param {array}  A - array of all possible answers. These will be referenced by index from the question object.
- * @param {string} questionNumber - Question ?1 of ?2 where ?! represents the current question and ?2 represents total questions. 
+ * @param {string} questionNumber - Represents the current question number. NOTE: question array index is questionNumber-1
  * @return {html}  question
  */
 var createQuestion = function(Q,A,questionNumber){
-  // var questionNumber_arr = questionNumber.split(" ");
-  // var question_index = questionNumber-1;
+  var question = $("<li>").text(Q.question).addClass("list-group-item question");
+  var answer_block = $("<div>").addClass("row");
 
-  // questionNumber_arr.forEach(function(value,index){
-  //   if(value === "of"){
-  //     question_index = questionNumber_arr[index-1];
-  //   }
-  // });
-
-  var question = $("<li class='list-group-item question'>");
-  question.text(Q.question);
-
-  var answer_block = $("<div class='row'>");
-  var answer_image = $("<img class='img-thumbnail rounded-circle'>");
+  // 1.4.1 ANSWER IMAGE
+  var answer_image = $("<img>").addClass("img-thumbnail rounded-circle");
   answer_image.attr("src",Q.image);
   answer_image.attr("alt",Q.topic+" - "+Q.subtopic);
-  answer_image = $("<div class='col-4 align-self-center text-right answer_image'><span class='image_wrap'>").html(answer_image);
+  answer_image = $("<span>").addClass("image_wrap").html(answer_image);
+  answer_image = $("<div>").addClass("col-4 align-self-center text-right answer_image").html(answer_image);
   answer_block.append(answer_image);
 
-  var answers = $("<div class='col-8 answers'>");
-  answers.append("<ul class='list-group'>");
+  // 1.4.2 POSSIBLE ANSWERS
+  var answers = $("<div>").addClass("col-8 answers");
+  answers.append($("<ul>").addClass("list-group"));
 
   var random_answer_index = generateRandomNumber(0,Q.answer.length);
   var possible_answers = [];
   possible_answers.push(Q.answer[random_answer_index]);
   possible_answers = $.merge(possible_answers,Q.wrong);
 
-  // Shuffle the possible answers so it's not always the first one. 
+  // Shuffle the possible answers so the correct answer is not always the first one. 
   possible_answers = shuffle_array(possible_answers);
 
   for(var i=0; i < possible_answers.length; i++ ){
-    var answer_option = $("<li class='list-group-item'><div class='custom-control custom-checkbox mr-sm-2'>");
+    var answer_option = $("<li>").addClass("list-group-item");
+    answer_option.html($("<div>").addClass("custom-control custom-checkbox mr-sm-2"));
 
-    // See if question has already been graded
-    //SUBMITTED_ANSWERS[current_question_index] = submitted_answer;
-    if(SCORE[questionNumber-1] === undefined){
-      answer_option.find(".custom-control").append("<input type='checkbox' class='custom-control-input' id='answer"+i+"' data-answer-index="+possible_answers[i]+" />");  
-      answer_option.find(".custom-control").append("<label class='custom-control-label' for='answer"+i+"' data-answer-index="+possible_answers[i]+">"+A[possible_answers[i]]+"</label>");
-      
-    }else{ // This question has already been graded
-      // console.log("Question "+ questionNumber + " has already been graded");
+    var answer_input = $("<input>").attr("type","checkbox").addClass("custom-control-input").attr("id","answer"+i).attr("data-answer-index",possible_answers[i]);
+    var answer_label = $("<label>").addClass("custom-control-label").attr("for","answer"+i).attr("data-answer-index",possible_answers[i]).text(A[possible_answers[i]]);
+     
+    // 1.4.3 See if question has already been graded
+    if(SCORE[questionNumber-1] !== undefined){
+      // This question has already been graded
+      answer_input.attr("disabled","true");
+
       if(possible_answers[i] === SUBMITTED_ANSWERS[questionNumber-1]){
-        answer_option.find(".custom-control").append("<input type='checkbox' class='custom-control-input' id='answer"+i+"' data-answer-index="+possible_answers[i]+" disabled='true' checked='true' />");
+        // Users Submitted Answer
+        answer_input.attr("checked","true");
         
-        if(QUESTIONS[questionNumber-1].answer.indexOf(SUBMITTED_ANSWERS[questionNumber-1]) !== -1){ // They got it right!
-          answer_option.find(".custom-control").append("<label class='custom-control-label right-answer' for='answer"+i+"' data-answer-index="+possible_answers[i]+">"+A[possible_answers[i]]+"</label>");
+        if(QUESTIONS[questionNumber-1].answer.indexOf(SUBMITTED_ANSWERS[questionNumber-1]) !== -1){ 
+          // They got it right!
+          answer_label.addClass("right-answer");
           
         }else{ // They got it wrong
-          answer_option.find(".custom-control").append("<label class='custom-control-label wrong-answer' for='answer"+i+"' data-answer-index="+possible_answers[i]+">"+A[possible_answers[i]]+"</label>");
+          answer_label.addClass("wrong-answer");
         }
-      }else{
-        answer_option.find(".custom-control").append("<input type='checkbox' class='custom-control-input' id='answer"+i+"' data-answer-index="+possible_answers[i]+" disabled='true'/>");
-        
-        if(QUESTIONS[questionNumber-1].answer.indexOf(possible_answers[i]) !== -1){ // This is the correct Answer
-          answer_option.find(".custom-control").append("<label class='custom-control-label right-answer' for='answer"+i+"' data-answer-index="+possible_answers[i]+">"+A[possible_answers[i]]+"</label>");
-        
-        }else{ // This is not the correct answer
-          answer_option.find(".custom-control").append("<label class='custom-control-label' for='answer"+i+"' data-answer-index="+possible_answers[i]+">"+A[possible_answers[i]]+"</label>");
+
+      }else{ // NOT their answer
+        if(QUESTIONS[questionNumber-1].answer.indexOf(possible_answers[i]) !== -1){ 
+          // This is the correct Answer
+          answer_label.addClass("right-answer");  
         }
       }
-    }
-
+    } // END if already graded
+    
+    answer_option.find(".custom-control").append(answer_input, answer_label);  
     answers.find(".list-group").append(answer_option);
-  }
+  } // END for(var i=0; i < possible_answers.length; i++ ){
 
+  // 1.4.4 COMBINED Answer Image with Possible Answers
   answer_block.append(answers);
-  answer_block = $("<li class='list-group-item answer_block'>").append(answer_block);
+  answer_block = $("<li>").addClass("list-group-item answer_block").append(answer_block);
 
-  question = $("<ul class='list-group text-left' >").append(question);
-
-  var q_counter = "<li class='list-group-item question_counter' data-start='" + (questionNumber - 1) + "' data-end='"+ (QUESTIONS.length-1) + "'>Question "+ questionNumber +" of "+ QUESTIONS.length +"</li>";
+  // 1.4.5 COMBINED Answer Block with the question
+  question = $("<ul>").addClass("list-group text-left").append(question);
+  var q_counter = $("<li>").addClass("list-group-item question_counter").attr("data-start",(questionNumber - 1)).attr("data-end",(QUESTIONS.length-1)).text("Question "+ questionNumber +" of "+ QUESTIONS.length);
   question.prepend(q_counter);
   question.append(answer_block);
 
@@ -182,34 +195,45 @@ var createQuestion = function(Q,A,questionNumber){
 function checkAnswer(){
   var current_question_index = $(".question_answers .question_counter").data("start");
   var submitted_answer = false;
+
+  /**
+   * 1.5.1 Fetch Submitted Answer
+   * by looping through the input elements. 
+   **/
   $(".question_answers .answers .custom-control-input").each(function(){
     if($(this).is(':checked')){
-      submitted_answer = $(this).data("answer-index");
+      submitted_answer = parseInt($(this).data("answer-index"));
     }
 
-    $(this).attr("disabled",true);
+    // Immediately disable all possible answers while grading
+    $(this).attr("disabled",true); 
   });
 
-  // Search Answers Array for submitted answer
+  /**
+   * 1.5.2 See if the answer is right
+   * by searching the Answers array for the submitted answer
+   **/
   var matchFound = false;
-  QUESTIONS[current_question_index].answer.forEach(function(value,index){
-    if(submitted_answer === value){
-      matchFound = true;
-    }
-  });
+  if(QUESTIONS[current_question_index].answer.indexOf(submitted_answer) !== -1){
+    matchFound = true;
+  }
 
+  // 1.5.3 Save Results weather it was right or wrong.
   SCORE[current_question_index] = matchFound;
   SUBMITTED_ANSWERS[current_question_index] = submitted_answer;
 
-  // Update HTML to reflect right and wrong answers
+  /**
+   * 1.5.4 Update HTML to reflect right and wrong answers
+   * by looping through the label elements.
+   */
   $(".question_answers .answer_block .custom-control-label").each(function(){
-    console.log(matchFound + "&" + $(this).attr("data-answer-index") + " === " +submitted_answer);
     if(matchFound === true && parseInt($(this).attr("data-answer-index")) === submitted_answer){
       $(this).addClass("right-answer");
-      console.log("yep they got it right. "+ $(this).attr("data-answer-index") + " === " +submitted_answer);
+
     }else{ // They got the answer wrong so we need to show right and wrong answers
-      // Their answer which was wrong
+
       if(parseInt($(this).attr("data-answer-index")) === submitted_answer){
+        // THIS would be their answer which was wrong
         $(this).addClass("wrong-answer");
       }
       
@@ -220,7 +244,10 @@ function checkAnswer(){
     }
   });
 
-  // var total_score = parseInt($("#quiz_score").data("score"));
+  /**
+   * 1.5.5 Calculate the score so far
+   * by creating filtered array that only represents completed answers.
+   */
   var filtered_score = SCORE.filter(function(el){
     return el != null;
   });
@@ -237,16 +264,11 @@ function checkAnswer(){
 
   var total_score = (right_answers/filtered_score.length)*100;
   total_score = total_score.toFixed(0);
-
+  
+  // Updated CIRCLE LOADER 
   QUIZ_PROGRESS.setPercent(total_score).draw();
-  // var progress = ((current_question_index+1)/QUESTIONS.length)*100;
-  // setTimeout(loader.setPercent(50).draw,5 * 1000);
 
-  // $("#quiz_score").data("score",total_score);
-  // $("#quiz_score").text(total_score+"%");
-  // runEffect($("#quiz_score"));
-
-  // PROGRESS BARS
+  // Update PROGRESS BARS
   var right_score = ((right_answers/SCORE.length)*100).toFixed(0) + "%";
   var wrong_score = ((wrong_answers/SCORE.length)*100).toFixed(0) + "%";
   $("#game_progress .bg-success").html(right_score + " right");
@@ -254,10 +276,11 @@ function checkAnswer(){
   $("#game_progress .bg-danger").html(wrong_score + " wrong");
   $("#game_progress .bg-danger").animate({width: wrong_score},"slow");
 
-}// END checkAnswer()
+} // END checkAnswer()
 
 /**
  * 1.6 nextQuestion
+ * Moves to the next question
  */
 function nextQuestion(){
   // Get Current Question information.
@@ -272,6 +295,7 @@ function nextQuestion(){
 
 /**
  * 1.7 previousQuestion
+ * Moves to the previous question
  */
 function previousQuestion(){
   // Get Current Question information.
@@ -285,16 +309,57 @@ function previousQuestion(){
 }
 
 /**
+ * 1.8 newGame
+ */
+function newGame(){
+  // 1.8.1 Reset Globals
+  SCORE = new Array(QUESTIONS.length);             
+  SUBMITTED_ANSWERS = new Array(QUESTIONS.length);
+
+  // 1.8.2 Generate the first question
+  QUESTIONS = shuffle_array(QUESTIONS); // Make sure to only shuffle this ONCE
+  var q = createQuestion(QUESTIONS[0],ANSWERS,1);
+  $(".question_answers").empty();
+  $(".question_answers").append(q);
+
+  /**
+   * 1.8.3 Set Quiz Progress to 0%
+   * ClassyLoader depends on the following script,
+   * <script src="assets/javascript/jquery.classyloader.min.js" type="text/javascript"></script>
+   */
+  QUIZ_PROGRESS.ClassyLoader({
+    animate: false,
+    percentage: 0,
+    speed: 20,
+    fontColor: '#1E2022',
+    fontSize: '50px',
+    diameter: 80,
+    lineColor: '#0092CA',
+    remainingLineColor: 'rgba(0, 146, 202, 0.3)',
+    lineWidth: 10
+  });
+
+  // 1.8.3 Reset Progress Bars
+  $("#game_progress .bg-success").html("");
+  $("#game_progress .bg-success").css("width",0);
+  $("#game_progress .bg-danger").html("");
+  $("#game_progress .bg-danger").css("width",0);
+}
+
+/**
  * 2. Document Ready
  */
-$(document).ready(function () {
+$(document).ready(function() {
   // 2.1 Generate the first question
   QUESTIONS = shuffle_array(QUESTIONS); // Make sure to only shuffle this ONCE
   var q = createQuestion(QUESTIONS[0],ANSWERS,1);
   $(".question_answers").append(q);
 
-  // 2.2 Make so only one question can be selected.
-  $('.custom-control-input').change(function () {
+  /**
+   * 2.2 Make so only one question can be selected.
+   * which must be done through event delegation using .on() delegated-events approach for this to work after dom manipulation.
+   */
+  $('.question_answers').on('change', '.custom-control-input', function() {
     $('.custom-control-input').prop('checked', false);
     $(this).prop('checked', true);
   });
@@ -317,16 +382,20 @@ $(document).ready(function () {
   });
 
   // 2.4 Set Up Clickable elements
-  $(".check_answer").on("click",function(){
+  $(".check_answer").click(function(){
     checkAnswer();
   });
 
-  $(".next_question").click("click",function(){
+  $(".next_question").click(function(){
     nextQuestion();
   });
 
-  $(".previous_question").click("click",function(){
+  $(".previous_question").click(function(){
     previousQuestion();
   });
 
-});
+  $("#new-game").click(function(e){
+    e.preventDefault();
+    newGame();
+  });
+}); // END $(document).ready(function() {
